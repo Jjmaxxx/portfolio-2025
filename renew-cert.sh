bebf8d701a76d7631ebf71b9e536412c5dbfbfe8
Justin Lee <justlee888@gmail.com>
2025-06-26 19:56:02 -0700

    fix bash scripts

--- renew-cert.sh
+++ renew-cert.sh
@@ -2,8 +2,8 @@
 
 cd ~/projects/portfolio-2025 || exit 1
 
-sudo docker compose -f docker-compose.prod.yml down
+sudo docker compose -f docker-compose.prod.yml down --remove-orphans
 
-sudo docker compose -f docker-compose.cert.yml up --abort-on-container-exit
+sudo docker compose -f docker-compose.cert.yml up --abort-on-container-exit --remove-orphans
 
-sudo docker compose -f docker-compose.prod.yml up -d
+sudo docker compose -f docker-compose.prod.yml up -d --remove-orphans
